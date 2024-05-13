0c1ee4998879cde0f5bae2dde9831b204f5a5919
eramkhangit <eramkhan3747@gmail.com>
2024-05-13 13:29:24 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { CustomTabPanel } from '../Component/CustomTabPanel'
+import BasicTabs,{ CustomTabPanel } from './Component/CustomTabPanel'
+import  {FullWidthTabs,TabPanel} from './Component/TabPanel'
+import ScrollableTabsButtonForce from './Component/ScrollableBtn'
 
 
 function App() {
@@ -10,7 +12,10 @@ function App() {
 
   return (
     <>
-   < CustomTabPanel />
+   < BasicTabs  />
+   <br />
+   < FullWidthTabs />
+   <ScrollableTabsButtonForce />
     </>
   )
 }
