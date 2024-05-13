b3225360ae3597d13369d147596e6fe4a1297f99
eramkhangit <eramkhan3747@gmail.com>
2024-05-13 13:36:09 +0530

    Third

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import BasicTabs,{ CustomTabPanel } from './Component/CustomTabPanel'
-import  {FullWidthTabs,TabPanel} from './Component/TabPanel'
 import ScrollableTabsButtonForce from './Component/ScrollableBtn'
 
 
@@ -14,7 +13,6 @@ function App() {
     <>
    < BasicTabs  />
    <br />
-   < FullWidthTabs />
    <ScrollableTabsButtonForce />
     </>
   )
